b57973daf02a4c24a68e9fbc0bfa20a871132b40
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-11-21 05:10:03 +0900

    implement ALU module

--- src/main/scala/Core.scala
+++ src/main/scala/Core.scala
@@ -24,16 +24,29 @@ class DMemIO extends Bundle {
 }
 
 
-class ALUIO extends Bundle {
-  val tag = UInt(INPUT, width = 4)
-  val a = UInt(INPUT, width = 32)
-  val b = UInt(INPUT, width = 32)
-  val c = UInt(OUTPUT, width = 32)
-}
-
 class ALU extends Module {
 
-  val io = new ALUIO()
+  val io = new Bundle {
+    val tag = UInt(INPUT, width = 4)
+    val a = UInt(INPUT, width = 32)
+    val b = UInt(INPUT, width = 32)
+    val c = UInt(OUTPUT, width = 32)
+  }
+
+  val (tag, a, b) = (io.tag, io.a, io.b)
+
+  val vadd = a + b
+  val vsub = a - b
+  val vsll = a << b
+  val vsrl = a >> b
+  val vsra = a.toSInt >> b
+  val vand = a & b
+  val vior = a | b
+  val vxor = a ^ b
+
+  val cases = Seq(vadd, vsub, vsll, vsrl, vsra, vand, vior, vxor).zipWithIndex.map { case (v, i) => UInt(i) -> v }
+
+  io.c := MuxLookup(tag, UInt(0), cases)
 
 }
 
