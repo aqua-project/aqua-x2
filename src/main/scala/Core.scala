7046801ae57055db08db56e07c89f592ae680b69
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-11-20 16:39:21 +0900

    initial commit

---
+++ src/main/scala/Core.scala
@@ -0,0 +1,21 @@
+import Chisel._
+
+class CPU extends Module {
+
+  val io = new Bundle {
+  }
+
+}
+
+class CPUTests(c: CPU) extends Tester(c) {
+  step(1)
+}
+
+object CPU {
+  def main(args: Array[String]) {
+    chiselMainTest(args, () => Module(new CPU)) { c =>
+      new CPUTests(c)
+    }
+  }
+}
+
