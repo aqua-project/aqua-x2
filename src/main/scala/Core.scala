e40fc003af20f5d0cc5cb5b2f46ae204d175464a
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-11-21 08:42:21 +0900

    detect data hazard and control hazard by hw

--- src/main/scala/Core.scala
+++ src/main/scala/Core.scala
@@ -69,21 +69,24 @@ class Core extends Module {
   }
 
 
-  /** Hazard */
+  // Forward Declarations
 
   val pc_src = Reg(Bool())
   val pc_addr = Reg(UInt(width = 32))
 
-  val stop_fetch = !io.imem.ready || !io.dmem.ready
-  val stop_decode = !io.dmem.ready
-  val stop_execute = !io.dmem.ready
+  val ex_reg_write = Reg(Bool())
+  val ex_reg_dest = Reg(UInt(width = 5))
+
+  val stop_fetch = Wire(Bool())
+  val stop_decode = Wire(Bool())
 
   def stop(regs: Data*) {
     for (reg <- regs) { reg := reg }
   }
 
 
-  /** Fetch */
+
+  /** Stage 1. Fetch */
 
   val nextpc = Reg(UInt(width = 32))
 
@@ -98,7 +101,7 @@ class Core extends Module {
   }
 
 
-  /** Decode & Read & Branch */
+  /** Stage 2. Decode & Read & Branch & Hazard */
 
   val idata = io.imem.data
   val opcode = idata(31, 26)
@@ -113,16 +116,19 @@ class Core extends Module {
   val disp_c = idata(20, 0).sext(32)
   val disp_s = idata(15, 0).sext(32)
 
+  // Read
   val rf = Mem(UInt(width = 32), 32)
   val rv1 = rf(rs1)
   val rv2 = rf(rs2)
 
+  // Decode
   val disp16 = Mux(idata(26), disp_s, disp_l)
   val raw_disp21 = Mux(idata(31, 29) === UInt("b101"), disp_c, disp_l)
   val disp21 = Mux(idata(26), raw_disp21 << 11, raw_disp21)
   val rv2_or_lit = Mux(idata(26), rv2, lit)
   val mem_valid = idata(31, 29) === UInt("b011")
-  val reg_write = idata(29) || (mem_valid && idata(28, 26) =/= UInt("b001"))
+  val rd_is_r31 = rd === UInt(31)
+  val reg_write = (idata(29) || (mem_valid && idata(28, 26) =/= UInt("b001"))) && !rd_is_r31
   val ucjmp = idata(31, 29) === UInt("b100")
   val cjmp = idata(31, 29) === UInt("b101")
   val ild = idata(31, 29) === UInt("b010")
@@ -139,15 +145,33 @@ class Core extends Module {
   val id_reg_write = Reg(next = reg_write)
   val id_reg_dest = Reg(next = rd)
 
-  val disp21x4 = raw_disp21 << 2
+  // Branch
   val eq0 = rv1 === UInt(0)
   val lt0 = rv1 < UInt(0)
   val le0 = rv1 <= UInt(0)
-  val cjcases = (0 until 7) map (UInt(_)) zip Seq(eq0, !eq0, lt0, le0, !le0, !lt0)
+  val cjcases = (0 until 6) map (UInt(_)) zip Seq(eq0, !eq0, lt0, le0, !le0, !lt0)
   val cjtaken = MuxLookup(idata(28, 26), Bool(false), cjcases)
+  val jtaken = ucjmp || (cjmp && cjtaken)
+  val disp21x4 = raw_disp21 << 2
   val npc_disp = nextpc + disp21x4
-  pc_src := ucjmp || (cjmp && cjtaken)
-  pc_addr := Mux(idata === UInt("b100001"), rv1, npc_disp)
+  val jaddr = Mux(idata === UInt("b100001"), rv1, npc_disp)
+  pc_src := jtaken
+  pc_addr := jaddr
+
+  // Hazard
+  val rawh1 = id_reg_write && (id_reg_dest === rs1 || id_reg_dest === rs2)
+  val rawh2 = ex_reg_write && (ex_reg_dest === rs1 || ex_reg_dest === rs2)
+  val rawh3 = io.dmem.resp.reg_write && (io.dmem.resp.reg_dest === rs1 || io.dmem.resp.reg_dest === rs2)
+  val data_hazard = rawh1 || rawh2 || rawh3    // We could make this more precise, but leave it for simplicity.
+  stop_fetch := !io.imem.ready || !io.dmem.ready || data_hazard
+  stop_decode := !io.dmem.ready
+  val kill_decode = data_hazard || pc_src
+
+  when (kill_decode) {
+    pc_src := Bool(false)
+    id_mem_valid := Bool(false)
+    id_reg_write := Bool(false)
+  }
 
   when (stop_decode) {
     stop(id_va, id_vb, id_tag, id_disp16, id_mdata)
@@ -159,7 +183,11 @@ class Core extends Module {
   // --- point of no return --- //
 
 
-  /** Execute */
+
+  val stop_execute = !io.dmem.ready
+
+
+  /** Stage 3. Execute */
 
   val alu = Module(new ALU)
   alu.io.tag := id_tag
@@ -172,14 +200,14 @@ class Core extends Module {
   val ex_addr = Reg(next = raddr)
   val ex_data = Reg(next = rdata)
   val ex_mem_valid = Reg(next = id_mem_valid)
-  val ex_reg_write = Reg(next = id_reg_write)
-  val ex_reg_dest = Reg(next = id_reg_dest)
+  ex_reg_write := id_reg_write
+  ex_reg_dest := id_reg_dest
 
   when (stop_execute) {
     stop(ex_addr, ex_data, ex_mem_valid, ex_reg_write, ex_reg_dest)
   }
 
-  /** Memory */
+  /** Stage 4. Memory */
 
   io.dmem.req.addr := ex_addr
   io.dmem.req.data := ex_data
@@ -188,7 +216,7 @@ class Core extends Module {
   io.dmem.req.reg_dest := ex_reg_dest
 
 
-  /** Write */
+  /** Stage 5. Write */
 
   when (io.dmem.resp.reg_write) { rf(io.dmem.resp.reg_dest) := io.dmem.resp.data }
 
